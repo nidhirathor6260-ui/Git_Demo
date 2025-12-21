411d75751524e9db66f154cebd081427e865cb2d
Nidhi Rathor <nidhirathor6260@gmail.com>
2025-12-21 20:50:14 +0530

    Modified

--- Color_change/script.js
+++ Color_change/script.js
@@ -1,15 +1,32 @@
-function setcolor(color){
-  let heading = document.getElementById('xyz')
-
+function setColor(color) {
+  let heading = document.getElementById('xyz');
+  // if(heading.style.color == color){
+  //   return;
+  // }
   heading.style.color = color;
-}
-function hide(){
-  let heading = document.getElementById('xyz')
   
+  let span = document.getElementById('counter');
+  let count = +span.innerHTML;
+  span.innerHTML = count + 1;
+
+  const allbuttons = document.getElementsByTagName("button")
+  for(let i = 0 ; i<allbuttons.length; i++){
+   const b = allbuttons[i];
+   b.disabled = false;
+ }
+  
+  const button = document.getElementById(color)
+  button.disabled = true;
+}
+
+ function hide() {
+  let heading = document.getElementById('xyz');
+
   heading.style.visibility = 'hidden';
 }
 
-function show(){
-  let heading = document.getElement.ById('xyz')
+function show() {
+  let heading = document.getElementById('xyz');
+
   heading.style.visibility = 'visible';
-}
+} 
