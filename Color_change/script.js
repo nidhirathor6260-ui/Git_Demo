77c83b9ebf5ebc025ee72a5c6f8cd5de9d495cf5
Nidhi Rathor <nidhirathor6260@gmail.com>
2025-12-21 19:55:44 +0530

    Color change by clicking on button

---
+++ Color_change/script.js
@@ -0,0 +1,15 @@
+function setcolor(color){
+  let heading = document.getElementById('xyz')
+
+  heading.style.color = color;
+}
+function hide(){
+  let heading = document.getElementById('xyz')
+  
+  heading.style.visibility = 'hidden';
+}
+
+function show(){
+  let heading = document.getElement.ById('xyz')
+  heading.style.visibility = 'visible';
+}
